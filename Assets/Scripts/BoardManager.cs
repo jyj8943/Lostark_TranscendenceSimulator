279cd341010936a8400e779cfbaff2cc92799a34
jyj8943 <jyj8943@gmail.com>
2024-09-30 02:48:33 +0900

    9/30 타일 및 카드 초기 구현

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameManager GM;
+
+    // 지금은 견갑 1단계 기준으로 작성. 나중에 단계를 선택하는 기능을 추가할 때 보완
+    private int maxRow = 6;
+    private int maxColumn = 6;
+    private float tileSize = 0.8f;
+    
     void Start()
+    {
+        GM = GameManager.Instance;
+        
+        GenerateGrid();
+    }
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateGrid()
     {
+        Vector3 gridOffset = new Vector3((maxColumn - 1) * tileSize / 2, (maxRow - 1) * tileSize / 2, 1f);
         
+        for (int row = 0; row < maxRow; row++)
+        {
+            for (int column = 0; column < maxColumn; column++)
+            {
+                Vector3 pos = new Vector3(column * tileSize, row * tileSize, 1f) - gridOffset;
+
+                var tempUnclickableTile = Instantiate(GM.normalTile, pos, quaternion.identity);
+                tempUnclickableTile.name = $"Tile_{row}_{column}";
+                tempUnclickableTile.transform.parent = transform;
+            }
+        }
+
+        transform.position = new Vector3(0f, 1.5f, 0f);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+
+    public GameObject normalTile;
+    public GameObject unclickableTile;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
